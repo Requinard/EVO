32a027fcde2995c8b316b2bb7b3b7403dfae49a5
David <d.diks@mail.blastedsolutions.com>
2014-10-09 12:43:18 +0200

    Added post models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+class Post(models.Model):
+    ## Metadata
+    owner = models.ForeignKey(User, verbose_name="Owner of this object")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    group = models.ForeignKey(Group)
+
+    ## Post data
+    post_body = models.TextField()
+    loc_lat = models.FloatField()
+    loc_long = models.FloatField()
+    loc_verbose = models.TextField()
+
+    def __str__(self):
+        return self.post_body
+
+    def get_location_num(self):
+        return "%s, %s" % (self.loc_lat, self.loc_long)
+
+    def get_post_comments(self):
+        return PostComment.objects.filter(post_related=self)
+
+class PostComment(models.Model):
+    ## Metadata
+    owner = models.ForeignKey(User, verbose_name="Owner of this object")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    post_related = models.ForeignKey(Post)
+
+    ## Post data
+    post_body = models.TextField()
+    loc_lat = models.FloatField()
+    loc_long = models.FloatField()
+    loc_verbose = models.TextField()
